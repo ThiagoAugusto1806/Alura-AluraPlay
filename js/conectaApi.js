11defb69d9dc3eb9c0189f9fcf8824e3b46c73fe
thiagoaugusto1806 <thiagoaugusto1806@gmail.com>
2024-08-01 15:52:45 +0000

    update da api

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,12 +1,12 @@
 async function listaVideos(){
-    const conexao = await fetch("http://localhost:3000/videos")
+    const conexao = await fetch("https://alura-play-api.vercel.app/videos")
     const conexaoConvertida = await conexao.json()
 
     return conexaoConvertida;
 }
 
 async function criaVideo(titulo, descricao, url, imagem){
-    const conexao = await fetch("http://localhost:3000/videos", {
+    const conexao = await fetch("https://alura-play-api.vercel.app/videos", {
         method: "POST",
         headers:{
             "Content-type": "application/json"
@@ -27,7 +27,7 @@ async function criaVideo(titulo, descricao, url, imagem){
 
 async function buscaVideo(termoDeBusca){
     console.log(termoDeBusca)
-    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexao = await fetch(`https://alura-play-api.vercel.app/videos?q=${termoDeBusca}`);
     console.log(conexao)
     const conexaoConvertida = await conexao.json()
     console.log(conexaoConvertida)

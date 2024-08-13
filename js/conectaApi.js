fa84153eb00ca2b0f30a998365ee0a23d06bc60a
ThiagoAugusto1806 <thiagoaugusto1806@gmail.com>
2024-08-12 21:35:01 -0300

    Update Responsividade

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,41 +1,48 @@
-async function listaVideos(){
-    const conexao = await fetch("https://alura-play-api.vercel.app/videos")
-    const conexaoConvertida = await conexao.json()
+async function listaVideos() {
+  const conexao = await fetch(
+    "https://my-json-server.typicode.com/ThiagoAugusto1806/AluraPlay-API/videos"
+  );
+  const conexaoConvertida = await conexao.json();
 
-    return conexaoConvertida;
+  return conexaoConvertida;
 }
 
-async function criaVideo(titulo, descricao, url, imagem){
-    const conexao = await fetch("https://alura-play-api.vercel.app/videos", {
-        method: "POST",
-        headers:{
-            "Content-type": "application/json"
-        },
-        body:JSON.stringify({
-            titulo: titulo,
-            descricao: `${descricao} mil visualizações`,
-            url: url,
-            imagem: imagem
-        })
-    })
-    if(!conexao.ok){
-        throw new Error("Não foi possivel enviar o video")
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch(
+    "https://my-json-server.typicode.com/ThiagoAugusto1806/AluraPlay-API/videos",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        titulo: titulo,
+        descricao: `${descricao} mil visualizações`,
+        url: url,
+        imagem: imagem,
+      }),
     }
-    const conexaoConvertida = await conexao.json()
-    return conexaoConvertida
+  );
+  if (!conexao.ok) {
+    throw new Error("Não foi possivel enviar o video");
+  }
+  const conexaoConvertida = await conexao.json();
+  return conexaoConvertida;
 }
 
-async function buscaVideo(termoDeBusca){
-    console.log(termoDeBusca)
-    const conexao = await fetch(`https://alura-play-api.vercel.app/videos?q=${termoDeBusca}`);
-    console.log(conexao)
-    const conexaoConvertida = await conexao.json()
-    console.log(conexaoConvertida)
-    return conexaoConvertida;
+async function buscaVideo(termoDeBusca) {
+  console.log(termoDeBusca);
+  const conexao = await fetch(
+    `https://my-json-server.typicode.com/ThiagoAugusto1806/AluraPlay-API/videos?q=${termoDeBusca}`
+  );
+  console.log(conexao);
+  const conexaoConvertida = await conexao.json();
+  console.log(conexaoConvertida);
+  return conexaoConvertida;
 }
 
 export const conectaApi = {
-    listaVideos, 
-    criaVideo, 
-    buscaVideo
-}
+  listaVideos,
+  criaVideo,
+  buscaVideo,
+};
--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,18 +1,17 @@
 import { conectaApi } from "./conectaApi.js";
 
-const formulario = document.querySelector('[data-formulario]')
+const formulario = document.querySelector("[data-formulario]");
 
-async function criarVideo(evento){
-    evento.preventDefault();
+async function criarVideo(evento) {
+  evento.preventDefault();
 
-    const imagem = document.querySelector('[data-imagem]').value;
-    const url = document.querySelector('[data-url]').value;
-    const titulo = document.querySelector('[data-titulo]').value;
-    const descricao = Math.floor(Math.random()*10).toString();
-    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+  const imagem = document.querySelector("[data-imagem]").value;
+  const url = document.querySelector("[data-url]").value;
+  const titulo = document.querySelector("[data-titulo]").value;
+  const descricao = Math.floor(Math.random() * 10).toString();
+  await conectaApi.criaVideo(titulo, descricao, url, imagem);
 
-    window.location.href = "./pages/envio-concluido.html";
-    
+  window.location.href = "./envio-concluido.html";
 }
 
-formulario.addEventListener("submit", evento => criarVideo(evento));
+formulario.addEventListener("submit", (evento) => criarVideo(evento));
